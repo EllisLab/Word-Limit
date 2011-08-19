3d0d425556e3463f834ab11ea9a622b8a7fa69fa
Derek Jones <derek.jones@ellislab.com>
2011-08-19 14:55:08 -0500

    relicensed using MIT (X11) License

--- pi.word_limit.php
+++ pi.word_limit.php
@@ -1,5 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+Copyright (C) 2004 - 2011 EllisLab, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ELLISLAB, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of EllisLab, Inc. shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from EllisLab, Inc.
+*/
+
 $plugin_info = array(
 						'pi_name'			=> 'Word Limiter',
 						'pi_version'		=> '1.1',
@@ -16,7 +41,7 @@ $plugin_info = array(
  * @package			ExpressionEngine
  * @category		Plugin
  * @author			ExpressionEngine Dev Team
- * @copyright		Copyright (c) 2004 - 2009, EllisLab, Inc.
+ * @copyright		Copyright (c) 2004 - 2011, EllisLab, Inc.
  * @link			http://expressionengine.com/downloads/details/word_limiter/
  */
 
